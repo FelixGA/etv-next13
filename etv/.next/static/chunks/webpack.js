69e9490da116210808d6c2c22fd71dd2a272d525
igalv <vilenskyigal@gmail.com>
2021-12-16 09:58:49 +0100

    test

--- etv/.next/static/chunks/webpack.js
+++ etv/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1c8e4dda20d1377"; }
+/******/ 		__webpack_require__.h = function() { return "1fec7866fb7dc591"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
