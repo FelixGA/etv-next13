3bda72fa571691ca6e1621092b4ef56b2d70654b
angelo.p <mrpalaiologos@gmail.com>
2021-12-16 11:43:08 +0100

    tailwind

--- etv/.next/static/chunks/webpack.js
+++ etv/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a718e7d4ed74607c"; }
+/******/ 		__webpack_require__.h = function() { return "81dbb3b7b46ca1bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
