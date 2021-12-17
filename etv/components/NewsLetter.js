d82cbd805261d432ef04c82c1ad480e3ff545007
angelo.p <mrpalaiologos@gmail.com>
2021-12-17 12:03:16 +0100

    components

---
+++ etv/components/NewsLetter.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Newsletter = () => {
+  return (
+    <div class="newsletter-container">
+      <h2> Zum Newsletter anmelden</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
+        nisi vel consectetur euismod, nisi nisl euismod nisi, vel consectetur
+        nisi nisl euismod.
+      </p>
+      <div>
+        <label for="email">email</label>
+        <input type="email" placeholder="e-mail" /> <button>anmelden</button>
+      </div>
+    </div>
+  );
+};
+export default Newsletter;
