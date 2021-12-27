26a08d1754aad3455f159b57fc5cea94f3a4a1e5
angelo.p <mrpalaiologos@gmail.com>
2021-12-27 09:14:53 +0100

    responsive vol2

--- etv/.next/static/chunks/webpack.js
+++ etv/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "540e032892dc481a"; }
+/******/ 		__webpack_require__.h = function() { return "e6336fe323354166"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
