e45489f708a7774a8eae0cdba88f547b31333994
igalv <vilenskyigal@gmail.com>
2021-12-28 10:53:59 +0100

    filter bloch

--- etv/pages/comparePage.js
+++ etv/pages/comparePage.js
@@ -1,9 +1,14 @@
 import FilterBlock from "../components/FilterBlock";
+/* import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconLookup } from "../node_modules/@fortawesome/fontawesome-svg-core"; */
 
 export default function comparePage() {
   return (
     <div>
       <FilterBlock />
+      {/*  <FontAwesomeIcon icon={IconLookup} /> */}
+
+      {/*  <FontAwesomeIcon icon="fa-solid fa-mug-hot" /> */}
     </div>
   );
 }
