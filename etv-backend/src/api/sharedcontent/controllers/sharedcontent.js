63324a4263450509047f88e5830b6399fe71f5d6
Daniel Schindler <team@ari-motors.com>
2022-02-11 10:14:47 +0100

    Remove strapi.

--- etv-backend/src/api/sharedcontent/controllers/sharedcontent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  sharedcontent controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sharedcontent.sharedcontent');
--- etv-backend/src/api/sharedcontent/routes/sharedcontent.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sharedcontent router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sharedcontent.sharedcontent');
