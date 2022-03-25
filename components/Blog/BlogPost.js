62c36ef98c9dca552849e48c3edf1b6595766c41
Igal <vilenskyigal@gmail.com>
2022-03-25 12:14:09 +0100

    before pull- start day

--- components/Blog/BlogPost.js
+++ components/Blog/BlogPost.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+
 import styles from "./BlogPost.module.css";
 import { MDXRemote } from "next-mdx-remote";
 export default function BlogPost({ getBlogContext }) {
