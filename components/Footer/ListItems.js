8b265c7eafe990f01a8f676750a366861996ec02
AngelosPa <mrpalaiologos@gmail.com>
2022-04-06 16:28:43 +0200

    problematic auto fixed

--- components/Footer/ListItems.js
+++ components/Footer/ListItems.js
@@ -4,21 +4,23 @@ const ListItems = (props) => {
   props.itemsList;
   return (
     <ul className="flex flex-col justify-between items-center pb-8 pt-2 text-black px-2">
-      {props.itemsList?.map((blog, index) => (
-        <li className="my-1 flex justify-between items-center" key={index}>
-          <Link
-            href={
-              blog.category === "rechtlichesundkontakt"
-                ? `/${blog.slug}`
-                : blog.category === "referenzen"
-                ? `/magazin/${blog.slug}`
-                : `/magazin/reviews/${blog.slug}`
-            }
-          >
-            <a className="text-center text-sm xs:text-lg">{blog.title}</a>
-          </Link>
-        </li>
-      ))}
+      {props.itemsList
+        ?.map((blog, index) => (
+          <li className="my-1 flex justify-between items-center" key={index}>
+            <Link
+              href={
+                blog.category === "rechtlichesundkontakt"
+                  ? `/${blog.slug}`
+                  : blog.category === "referenzen"
+                  ? `/magazin/${blog.slug}`
+                  : `/magazin/reviews/${blog.slug}`
+              }
+            >
+              <a className="text-center text-sm xs:text-lg">{blog.title}</a>
+            </Link>
+          </li>
+        ))
+        .splice(0, 5)}
     </ul>
   );
 };
