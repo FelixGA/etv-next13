5a8d789f9fd8a33b5978201a5f80eb2df0a24803
Igal <vilenskyigal@gmail.com>
2022-05-19 14:04:39 +0200

    newsletter restructurw, top logo, magazin fix

--- components/Homepage/NewsLetter.js
+++ components/Homepage/NewsLetter.js
@@ -2,6 +2,7 @@ import { MDXRemote } from "next-mdx-remote";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import TextInput from "../core/TextInput";
 const NewsLetter = ({ getMarkdownContext }) => {
   const emailRegex = RegExp(
     /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
@@ -31,40 +32,34 @@ const NewsLetter = ({ getMarkdownContext }) => {
   const onError = (errors, e) => console.log("errors", errors, e);
 
   return (
-    <div className="flex flex-col flex-wrap items-center justify-center px-10 pt-6 pb-12 newsletter-container bg-grey-lightest">
+    <div className="flex flex-col flex-wrap items-center justify-center px-10 pt-6 pb-10 bg-grey-lightest">
       <div className="flex flex-col justify-center sm:px-4 py-4 xl:w-[1000px] custom-text text-grey-darker">
         <MDXRemote {...getMarkdownContext.newsletter} />
       </div>
       <div>
-        <p
-          className={
-            send
-              ? "hidden"
-              : "w-full align-left text-blue-lighter font-bold pb-1 pl-1"
-          }
-        >
-          Email <span className="text-red-500">*</span>
-        </p>
-
         <form
           method="POST"
           onSubmit={handleSubmit(onSubmit, onError)}
-          className={send ? "hidden" : "flex flex-col xs:flex-row"}
+          className={
+            send && !errors.emailInput
+              ? "hidden"
+              : "flex flex-col xs:flex-row mt-4 relative"
+          }
         >
-          <input
-            content-type="email"
-            placeholder="E-Mail"
-            id="emailInput"
-            type="email"
-            {...register("emailInput", {
-              required: true,
-              pattern: emailRegex,
-            })}
-            className="pl-2 text-lg rounded-sm w-52 h-14 sm:w-full "
+          <TextInput
+            style={`${errors.emailInput && " focus:border-red-500 "}`}
+            placeholder={"z.B. max@muster.com"}
+            register={register}
+            label={"Email:"}
+            id={"emailInput"}
+            type={"string"}
+            registerData={"emailInput"}
+            required={true}
+            pattern={emailRegex}
           />
-          <p className="text-red-500">
+          {/* <p className="text-red-200">
             {errors.emailInput && "Email ist erforderlich"}
-          </p>
+          </p> */}
           <button
             onClick={() => {
               !errors.emailInput && watch().emailInput.length > 0
@@ -72,18 +67,20 @@ const NewsLetter = ({ getMarkdownContext }) => {
                 : setSend(false);
             }}
             type="submit"
-            className="px-6 mt-2 font-bold text-white transition rounded-md bg-blue-dark xs:mt-0 hover:bg-blue-light text-base sm:rounded-r-lg xs:rounded-l-none h-14"
+            className="relative px-6 text-base font-bold text-white transition rounded-md lg:top-3 xs:mt-2 bg-blue-dark lg:mt-6 hover:bg-blue-light sm:rounded-r-lg xs:rounded-l-none h-14"
           >
-            anmelden
+            Anmelden
           </button>
         </form>
         <div
           className={
-            send ? "flex h-fit justify-center items-center w-full " : "hidden"
+            send && !errors.emailInput
+              ? "flex h-fit justify-center items-center w-full "
+              : null
           }
         >
-          <p className="w-full px-8 mt-4 sm:p-8 sm:w-2/3 sm:text-lg">
-            Vielen Dank! Das Formular wurde erfolgreich übermittelt.
+          <p className="text-red-500">
+            {errors.emailInput && "Email ist erforderlich"}
           </p>
         </div>
       </div>
