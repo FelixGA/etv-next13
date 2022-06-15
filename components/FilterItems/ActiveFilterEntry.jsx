431a38f9901bc099a69d0c413f1219830e9acc32
AngelosPa <mrpalaiologos@gmail.com>
2022-06-15 18:22:03 +0200

    filter boxes

--- components/FilterItems/ActiveFilterEntry.jsx
+++ components/FilterItems/ActiveFilterEntry.jsx
@@ -12,6 +12,7 @@ import { useState, useEffect } from "react";
 function ActiveFilterEntry(props) {
   const { state, dispatch } = useStore();
   const [filterData, setFilterData] = useState([]);
+  const [herstellerData, setHerstellerData] = useState([]);
 
   useEffect(() => {
     if (
@@ -122,6 +123,17 @@ function ActiveFilterEntry(props) {
         image: image6,
       },
     ]);
+    setHerstellerData(
+      state?.brands.length
+        ? state?.brands.map((el) => {
+            return {
+              id: 1,
+              value: el,
+              image: image6,
+            };
+          })
+        : null
+    );
   }, [
     state?.prices,
     state?.rangeLithiums,
@@ -129,8 +141,8 @@ function ActiveFilterEntry(props) {
     state?.maxSpeeds,
     state?.chargingTimeLithiums,
     state?.categorys,
+    state?.brands,
   ]);
-
   return (
     <div
       className={
@@ -146,7 +158,7 @@ function ActiveFilterEntry(props) {
               ? "flex-1 h-10 py-2  bg-grey-lighter flex justify-between items-center "
               : "hidden"
           }
-          key={item.id}
+          key={index}
         >
           <div className="w-6 ml-2 ">
             <Image
@@ -176,6 +188,42 @@ function ActiveFilterEntry(props) {
           </div>
         </div>
       ))}
+      {herstellerData?.map((item, index) => (
+        <div
+          className={
+            item.value !== null
+              ? "flex-1 h-10 py-2  bg-grey-lighter flex justify-between items-center "
+              : "hidden"
+          }
+          key={index}
+        >
+          <div className="w-6 ml-2 ">
+            <Image
+              src={item.image}
+              alt="picture"
+              objectFit="cover"
+              width={24}
+              height={28}
+              layout="responsive"
+            />
+          </div>
+          <div className="text-base">{item.value}</div>
+
+          <div
+            onClick={() => {
+              // props.setShowAll(!props.showAll);
+              dispatch({
+                type: "brand",
+                data: state?.brands.filter((el) => el !== item.value),
+              });
+            }}
+            className={"w-3.5 my-auto mr-4 cursor-pointer"}
+          >
+            <AiOutlineClose size={20} />
+          </div>
+        </div>
+      ))}
+
       {/* REMOVE ALL FILTERS */}
       <div
         className={
@@ -184,7 +232,8 @@ function ActiveFilterEntry(props) {
           state?.loadingWeights.length ||
           state?.maxSpeeds.length ||
           state?.chargingTimeLithiums.length ||
-          state?.categorys.length
+          state?.categorys.length ||
+          state?.brands.length
             ? "flex justify-end items-start lg:items-end md:justify-start pr-2 h-10"
             : "hidden"
         }
@@ -215,6 +264,10 @@ function ActiveFilterEntry(props) {
             });
             dispatch({
               type: "category",
+              data: [],
+            });
+            dispatch({
+              type: "brands",
               data: [],
             });
           }}
