012a3ce93d78e5b8ee6c587a4039ba1ba208c908
FelixGA <89196842+FelixGA@users.noreply.github.com>
2023-01-16 14:35:49 +0100

    Cookiebanner (#75)
    * cookie
    
    * cookie
    
    * page prop aktiviert
    
    * console.log(pages) in news und getcontent.js wegen vercel fehler
    
    * props pages deaktiviert
    
    * manufacturer/index.js post und vehicle props raus
    
    * von npm auf yarn umgestellt, next-sitemap gebaut mit yarn build
    
    * nothing changed
    
    * ga cookies werden abgelehnt
    
    * banner vorbereitet
    
    * googleTag zu GTM-ID
    
    * link datenschutz angepasst
    
    * @phntms/next-gtm
    
    * position geändert und button raus
    
    Co-authored-by: Felix <garske@ari-motors.com>

--- components/core/CookieBanner.js
+++ components/core/CookieBanner.js
@@ -11,7 +11,7 @@ export default function CookieBanner(props) {
   console.log(cookiesAccepted);
 
   return (
-    <div className="absolute top-0 flex flex-wrap lg:flex-nowrap bg-neutral-400 p-4 lg:px-16">
+    <div className="fixed bottom-0 flex flex-wrap lg:flex-nowrap bg-neutral-400 p-4 lg:px-16">
       <div className="flex flex-col justify-center">
         <p className="text-xl text-white font-black my-4">
           Einverständniserklärung
@@ -19,14 +19,16 @@ export default function CookieBanner(props) {
         <p className="text-white">
           ARI Motors braucht für einzelne Datennutzungen Deine Einwilligung, um
           Dein Nutzererlebnis zu verbessern. Mit Klick auf "Akzeptieren" gibst
-          Du diese Einwilligung. Unsere Datenschutzerklärung findest Du
+          Du diese Einwilligung. Um abzulehnen, klicke <span onClick={(e) => {
+            setCookiesAccepted(false);
+          }}>hier</span>. Unsere Datenschutzerklärung findest Du
           <Link href="/dataprotection">
             <a className="font-bold"> hier.</a>
           </Link>
         </p>
         <div className="flex gap-4 self-end">
 
-        <Button1
+        {/* <Button1
           className="self-end w-full lg:w-52 mt-2"
           name="decline"
           onClick={(e) => {
@@ -34,7 +36,7 @@ export default function CookieBanner(props) {
           }}
         >
           Ablehnen
-        </Button1>
+        </Button1> */}
         <Button1
           className="mt-2 bg-yellow-300 text-black"
           name="accept"
