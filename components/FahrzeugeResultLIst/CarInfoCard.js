c6b30e489b07b30419d3be377cad51f6623e24c4
Felix <89196842+FelixGA@users.noreply.github.com>
2023-01-31 15:56:43 +0100

    leichttransporter page gefiltert, links gesetzt footer

--- components/FahrzeugeResultLIst/CarInfoCard.js
+++ components/FahrzeugeResultLIst/CarInfoCard.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 const CarInfoCard = ({ carItem, getCarsReview, objectFit }) => {
+  // console.log(carItem);
   const router = useRouter();
   return (
     <div className="px-4 2xl:px-0">
